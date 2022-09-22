a4d960f7442b47c329a05033862871d1bfc1a445
Konstantin Salyaev <104251150+Saljaev@users.noreply.github.com>
2022-09-22 17:15:30 +0300

    Time: 12 ms (71.48%), Space: 3.3 MB (70.40%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.go
+++ 12-integer-to-roman/12-integer-to-roman.go
@@ -1,23 +1,9 @@
 func intToRoman(num int) string {
-	s := map[string]int{
-		"M":  1000,
-		"CM": 900,
-		"D":  500,
-		"CD": 400,
-		"C":  100,
-		"XC": 90,
-		"L":  50,
-		"XL": 40,
-		"X":  10,
-		"IX": 9,
-		"V":  5,
-		"IV": 4,
-		"I":  1,
-	}
+	romanValue := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 	result := ""
 	roman := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 	index := 0
-	minus := s[roman[index]]
+	minus := romanValue[index]
 	for num > 0 {
 		if num-minus >= 0 {
 			num -= minus
@@ -25,7 +11,7 @@ func intToRoman(num int) string {
 
 		} else {
 			index++
-			minus = s[roman[index]]
+			minus = romanValue[index]
 		}
 
 	}
