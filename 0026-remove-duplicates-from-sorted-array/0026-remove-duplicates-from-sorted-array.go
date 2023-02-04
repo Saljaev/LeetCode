7c484de0f35c5b5b1c3c972a00397f22bb6bae33
Konstantin Salyaev <104251150+Saljaev@users.noreply.github.com>
2023-02-04 22:32:27 +0300

    Time: 11 ms (56.29%), Space: 4.3 MB (100.00%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+	i, j := 0, 1
+	for j < len(nums) {
+		if nums[i] == nums[j] {
+			j++
+		} else {
+			nums[i+1] = nums[j]
+			i++
+			j++
+		}
+	}
+	return i + 1
+}
