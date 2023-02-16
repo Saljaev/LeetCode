d307cbd4d29ea4ecf8fad9e5fdff2ea4f365ba64
Konstantin Salyaev <104251150+Saljaev@users.noreply.github.com>
2023-02-16 21:26:13 +0300

    Time: 6 ms (57.49%), Space: 2.9 MB (29.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,9 +1,9 @@
 func maxProfit(prices []int) int {
-	maxprofit := 0
-	for i := 0; i < len(prices)-1; i++ {
-		if prices[i+1] > prices[i] {
-			maxprofit+=prices[i+1]-prices[i]
-		}
-	}
-	return maxprofit
+    maxProfit:=0
+    for i:=0; i<len(prices)-1; i++ {
+        if prices[i+1] > prices[i]{
+            maxProfit+=prices[i+1]-prices[i]
+        }
+    }
+    return maxProfit
 }
